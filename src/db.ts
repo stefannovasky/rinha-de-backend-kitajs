b9807c9845b14fc37d3807e50527f248ba7ab47d
stefannovasky <stefannovasky@gmail.com>
2024-03-01 20:58:17 -0300

    add pg connection

---
+++ src/db.ts
@@ -0,0 +1,8 @@
+import postgres from "postgres";
+
+export const sql = postgres({
+  host: process.env.PG_HOST,
+  database: process.env.PG_DB,
+  user: process.env.PG_USER,
+  password: process.env.PG_PASSWORD,
+});
